3ce0abbda791684cccb00c240562445be4449d68
Ekaterina Voronina <vor@altlinux.org>
2023-11-16 16:17:57 +0400

    Initial commit

---
+++ place_files.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+BACKEND_DIR=/usr/share/alterator/backends
+DESKTOP_DIR=/usr/share/alterator/adttools
+ALTERATOR_DIR=/usr/share/alterator
+
+if test -d "$ALTERATOR_DIR"; then
+    for file in ./*
+    do
+        if test -f $file  ; then
+        case $file in
+        *.backend)
+            if  test -d "$BACKEND_DIR"; then
+                cp $file $BACKEND_DIR
+            else
+                echo "$BACKEND_DIR does not exist."
+            fi
+            ;;
+
+        *.desktop)
+            if  test -d "$DESKTOP_DIR"; then
+                cp $file $DESKTOP_DIR
+            else
+                mkdir $DESKTOP_DIR
+                cp $file $DESKTOP_DIR
+            fi
+            ;;
+      esac
+    fi
+done
+
+else
+    echo "Error: firstly download alterator."
+fi
+
+
+# # place files
+# if  test -d "$BACKEND_DIR"; then
+#     echo "$BACKEND_DIR exists."
+# else
+#     echo "$BACKEND_DIR does not exist."
+# fi
+#
+# if test -d "$DESKTOPR_DIR"; then
+#     echo "$DESKTOPR_DIR exists."
+# else
+#     echo "$DESKTOPR_DIR does not exist."
+# fi
+
---
+++ rtf.md
@@ -0,0 +1,5 @@
+#First step
+Correct desktop and backend files.
+
+#Second step
+excecute place_files script with sudo privilegies. 
